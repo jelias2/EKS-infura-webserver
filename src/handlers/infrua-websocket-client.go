14f62e687adeb1a5d6e997b7ce003d66aeda5c51
Jacob-Elias <jacob.elias@ibm.com>
2021-08-14 15:16:18 -0500

    Socket2 Socket working and refactored

---
+++ src/handlers/infrua-websocket-client.go
@@ -0,0 +1,132 @@
+package handlers
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+	"github.com/jelias2/infra-test/src/apis"
+	"go.uber.org/zap"
+)
+
+// WebSocketGetGasPrice
+func (h *Handler) WebSocketGetBlockNumber(w http.ResponseWriter, r *http.Request) {
+
+	getBlockBody, _ := json.Marshal(h.CreateRequestBody(apis.GetBlockNumber, []string{}))
+	message, ErrorResponse := h.WebSocketWriteAndRead(getBlockBody)
+	if ErrorResponse.Message != "" && ErrorResponse.StatusCode != 0 {
+		json.NewEncoder(w).Encode(ErrorResponse)
+		return
+	}
+	wsGetBlockNumberResponse := &apis.GetBlockNumberResponse{}
+	json.Unmarshal(message, wsGetBlockNumberResponse)
+	json.NewEncoder(w).Encode(wsGetBlockNumberResponse)
+}
+
+// WebSocketGetGasPrice
+func (h *Handler) WebSocketGetGasPrice(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	getBlockBody, _ := json.Marshal(h.CreateRequestBody(apis.GetGasPrice, []string{}))
+	message, ErrorResponse := h.WebSocketWriteAndRead(getBlockBody)
+	if ErrorResponse.Message != "" && ErrorResponse.StatusCode != 0 {
+		json.NewEncoder(w).Encode(ErrorResponse)
+		return
+	}
+	wsGetGasResponse := &apis.GetGasPriceResponse{}
+	json.Unmarshal(message, wsGetGasResponse)
+	json.NewEncoder(w).Encode(wsGetGasResponse)
+}
+
+// WebSocketGetGasPrice
+// GetBlockByNumber
+func (h *Handler) WebSocketGetBlockByNumber(w http.ResponseWriter, r *http.Request) {
+
+	var txdetails bool
+	w.Header().Set("Content-Type", "application/json")
+	formmattedRequest, validRequest, txdetails := h.ParseGetBlockByNumberRequest(r)
+	if !validRequest {
+		wsError := &apis.ErrorResponse{}
+		json.Unmarshal(formmattedRequest, wsError)
+		json.NewEncoder(w).Encode(wsError)
+		return
+	}
+
+	if txdetails {
+		json.NewEncoder(w).Encode(h.WebSocketGetBlockByNumberHandler(formmattedRequest, apis.GetBlockByNumberTxDetailsResponse{}))
+	}
+	json.NewEncoder(w).Encode(h.WebSocketGetBlockByNumberHandler(formmattedRequest, apis.GetBlockByNumberNoTxDetailsResponse{}))
+}
+
+func (h *Handler) WebSocketGetBlockByNumberHandler(body []byte, umarshallStruct interface{}) interface{} {
+	var message []byte
+	var errorResponse apis.ErrorResponse
+	message, errorResponse = h.WebSocketWriteAndRead(body)
+	if errorResponse.Message != "" && errorResponse.StatusCode != 0 {
+		return errorResponse
+	}
+
+	// Umarshall response into the type of the umarshallStruct
+	switch umarshallStruct.(type) {
+	case apis.GetBlockByNumberTxDetailsResponse:
+		wsResult := &apis.GetBlockByNumberTxDetailsResponse{}
+		json.Unmarshal(message, wsResult)
+		return wsResult
+	case apis.GetBlockByNumberNoTxDetailsResponse:
+		wsResult := &apis.GetBlockByNumberNoTxDetailsResponse{}
+		json.Unmarshal(message, wsResult)
+		return wsResult
+	default:
+		h.Log.Error("Improper Type")
+		return &apis.ErrorResponse{StatusCode: http.StatusBadRequest, Message: "Error Unmarshalling GetBlockResponse"}
+	}
+}
+
+// WebSocketGetTransactionByBlockNumberAndIndex
+func (h *Handler) WebSocketGetTransactionByBlockNumberAndIndex(w http.ResponseWriter, r *http.Request) {
+
+	w.Header().Set("Content-Type", "application/json")
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var getTxReq apis.GetTransactionByBlockNumberAndIndexRequest
+	if err := json.Unmarshal(reqBody, &getTxReq); err != nil {
+		h.Log.Error("Error unmarshalling GetBlockByNumberRequest", zap.Error(err))
+	}
+
+	if getTxReq.Block == "" || getTxReq.Index == "" {
+		json.NewEncoder(w).Encode(apis.MalformedRequestError)
+		return
+	}
+
+	getBlockTxIndex, _ := json.Marshal(h.CreateRequestBody(apis.GetTransactionByBlockNumberAndIndex, []string{getTxReq.Block, getTxReq.Index}))
+	message, errorResponse := h.WebSocketWriteAndRead(getBlockTxIndex)
+	if errorResponse.Message != "" && errorResponse.StatusCode != 0 {
+		json.NewEncoder(w).Encode(errorResponse)
+	}
+
+	wsGetTxByBlockAndIndexResp := &apis.GetTransactionByBlockNumberAndIndexResponse{}
+	json.Unmarshal(message, wsGetTxByBlockAndIndexResp)
+	json.NewEncoder(w).Encode(wsGetTxByBlockAndIndexResp)
+
+}
+
+func (h *Handler) WebSocketWriteAndRead(body []byte) ([]byte, apis.ErrorResponse) {
+	errorMessage := ""
+	statusCode := 0
+	err := h.WebSocket.WriteMessage(websocket.TextMessage, body)
+	if err != nil {
+		h.Log.Info("Error writing WebSocketGetGasPrice websocket message", zap.Error(err))
+		errorMessage = err.Error()
+		statusCode = http.StatusBadRequest
+	}
+	_, message, err := h.WebSocket.ReadMessage()
+	if err != nil {
+		h.Log.Info("Error reading WebSocketGetGasPrice websocket message", zap.Error(err))
+		errorMessage = err.Error()
+		statusCode = http.StatusBadRequest
+	}
+	errorResponse := apis.ErrorResponse{
+		StatusCode: statusCode,
+		Message:    errorMessage,
+	}
+	return message, errorResponse
+}
---
+++ src/handlers/socket2socket-handlers.go
@@ -0,0 +1,118 @@
+package handlers
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+	"github.com/jelias2/infra-test/src/apis"
+	"go.uber.org/zap"
+)
+
+func (h *Handler) Socket2socket(w http.ResponseWriter, r *http.Request) {
+
+	var infuraReq []byte
+	var ok bool
+	infuraClient, clientConn := h.UpgradeConnection(w, r)
+
+	for {
+		var err error
+
+		_, msg, err := clientConn.ReadMessage()
+		if err != nil {
+			h.Log.Error("Failed to read message from websocket client")
+			break
+		}
+		h.Log.Info("Recieved Websocket Message", zap.String("Message", string(msg)))
+
+		if bytes.Contains(msg, []byte("true")) || bytes.Contains(msg, []byte("false")) {
+			if infuraReq, ok = h.formatInfuraBooeanRequestMsg(msg); !ok {
+				clientConn.WriteMessage(websocket.TextMessage, infuraReq)
+				continue
+			}
+		} else {
+			if infuraReq, ok = h.formatInfuraRequestMsg(msg); !ok {
+				clientConn.WriteMessage(websocket.TextMessage, []byte("Failed to unmarshalll message client"))
+				continue
+			}
+		}
+
+		if ok {
+			clientResp := h.WriteAndReadToInfura(infuraClient, infuraReq)
+			h.Log.Info("Writing Client websocket message", zap.ByteString("Response", clientResp))
+			if err = clientConn.WriteMessage(websocket.TextMessage, clientResp); err != nil {
+				h.Log.Info("Error wrting client message", zap.Error(err))
+				break
+			}
+		}
+	}
+
+	infuraClient.Close()
+	clientConn.Close()
+}
+
+func (h *Handler) UpgradeConnection(w http.ResponseWriter, r *http.Request) (*websocket.Conn, *websocket.Conn) {
+	upgrader := websocket.Upgrader{}
+	clientConn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		json.NewEncoder(w).Encode(&apis.ErrorResponse{
+			StatusCode: http.StatusUpgradeRequired,
+			Message:    "Error Upgrading to WebSocket connection",
+		})
+	}
+
+	mainnetWebsocketEndpoint := h.Mainnet_websocket_endpoint
+	infuraClient, _, err := websocket.DefaultDialer.Dial(mainnetWebsocketEndpoint, nil)
+	if err != nil {
+		log.Fatal("Fatal Dial Error:", zap.Error(err))
+		clientConn.WriteMessage(websocket.CloseMessage, []byte("Failed to unmarshalll message client"))
+		clientConn.Close()
+	}
+	return infuraClient, clientConn
+}
+
+func (h *Handler) WriteAndReadToInfura(infuraClient *websocket.Conn, infuraReq []byte) []byte {
+	var err error
+	var infuraResp []byte
+	if err = infuraClient.WriteMessage(websocket.TextMessage, infuraReq); err != nil {
+		h.Log.Info("Error writing Infura websocket message", zap.Error(err))
+		return []byte("Error writing Infura websocket message")
+	}
+
+	if _, infuraResp, err = infuraClient.ReadMessage(); err != nil {
+		h.Log.Info("Error reading Infura websocket message", zap.Error(err))
+		return []byte("Error reading Infura websocket message")
+	}
+	return infuraResp
+
+}
+
+// eth_getBlockByNumber, eth_getBlockByHash contain boolean, have len 2 params array
+func (h *Handler) formatInfuraBooeanRequestMsg(body []byte) ([]byte, bool) {
+	body = bytes.Replace(body, []byte("true"), []byte("\"true\""), 1)
+	body = bytes.Replace(body, []byte("false"), []byte("\"false\""), 1)
+	reqBody := &apis.InfuraRequestBody{}
+	if err := json.Unmarshal(body, reqBody); err != nil {
+		h.Log.Info("Error Parsing boolean websocket message", zap.Error(err))
+		return []byte("Error Parsing boolean websocket message"), false
+	}
+	body = []byte(fmt.Sprintf(apis.BooleanRequestBodyTemplate, reqBody.Method, reqBody.Params[0], reqBody.Params[1]))
+	return body, true
+}
+
+func (h *Handler) formatInfuraRequestMsg(clientReq []byte) ([]byte, bool) {
+	var reqBody = &apis.InfuraRequestBody{}
+	var msg []byte
+	var err error
+	if err = json.Unmarshal(clientReq, reqBody); err != nil {
+		return []byte("Failed to unmarshall request message for Infura"), false
+	}
+
+	if msg, err = json.Marshal(h.CreateRequestBody(reqBody.Method, reqBody.Params)); err != nil {
+		return []byte("Failed to create formmated request message for Infura"), false
+	}
+	return msg, true
+}
